446af04b100e86930be03d29d2cec0b1c5057edc
Tejas <tejas.rai.phe17@iitbhu.ac.in>
2018-06-26 03:00:51 +0530

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,19 @@ var li=jQuery('<li></li>');
 li.text(`${message.from}: ${message.text}`);
 jQuery('#messages').append(li);
 });
+
+socket.on('newLocationMessage',function(message){
+  var li=jQuery('<li></li>');
+  var a=jQuery('<a target="_blank">My current location</a>');
+  li.text(`${message.from}: `);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
+
+jQuery('#message-form').on('submit',function(e){
+e.preventDefault();
 socket.emit('createMessage',{
   from:'Frank',
   text:'Hi'
@@ -18,8 +31,7 @@ socket.emit('createMessage',{
   console.log('Got it',data);
 });
 
-jQuery('#message-form').on('submit',function(e){
-e.preventDefault();
+
 
   socket.emit('createMessage',{
     from:'User',
@@ -28,3 +40,17 @@ e.preventDefault();
 
   });
 });
+var locationButton=jQuery('#send-location');
+locationButton.on('click',function(){
+if(!navigator.geolocation){
+  return alert('Geolocation not supported by your browser.');
+}
+  navigator.geolocation.getCurrentPosition(function(position){
+    socket.emit('createLocationMessage',{
+      latitude:position.coords.latitude,
+      longitude:position.coords.longitude
+    });
+  },function(){
+    alert('Unable to fetch location.');
+  });
+})
