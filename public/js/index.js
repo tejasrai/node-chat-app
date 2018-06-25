9b906215e3ba2c3f3f2573b723b07e1c7c755916
Tejas <tejas.rai.phe17@iitbhu.ac.in>
2018-06-25 07:07:16 +0530

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+var socket=io();
+socket.on('connect',function(){
+console.log('Connected to server');});
+socket.on('disconnect',function(){
+  console.log('Disconnected from server');
+  socket.emit('createMessage',{
+    from:'Andrew',
+    text:'Yup that works for me.'
+  });
+});
+socket.on('newMessage',function(message){
+console.log('newMessage',message);  
+});
--- server/server.js
+++ server/server.js
@@ -11,8 +11,16 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
   console.log('New user connected');
+  socket.emit('newMessage',{
+    from:'John',
+    text:'See you then',
+    createAt:123123
+  });
+  socket.on('createMessage',(message)=>{
+    console.log('createMessage',message);
+  });
   socket.on('disconnect',()=>{
-    console.log('User was disconnected')
+    console.log('User was disconnected');
   });
 });
 server.listen(3000,()=>{
